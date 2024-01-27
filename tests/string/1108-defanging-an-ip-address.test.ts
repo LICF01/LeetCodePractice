302a73f93c05b6996d9bc1bf5fb12eacacc553b0
LICF01 <lucascubilla@gmail.com>
2024-01-27 02:30:31 -0300

    Add more test cases

--- tests/string/1108-defanging-an-ip-address.test.ts
+++ tests/string/1108-defanging-an-ip-address.test.ts
@@ -1,12 +1,15 @@
 import { describe, it, expect } from "vitest";
 import { defangIPaddr } from "../../src/string/1108-defanging-an-ip-address";
 
-describe("defangIP", () => {
-  it('should replace all periods in an IP address with "[.]"', () => {
-    const input: string = "1.1.1.1";
-    const expectedOutput: string = "1[.]1[.]1[.]1";
-
-    const result: string = defangIPaddr(input);
-    expect(result).toBe(expectedOutput);
+describe("1108 - Defainging an IP address", () => {
+  const testCases = [
+    { input: "1.1.1.1", expected: "1[.]1[.]1[.]1" },
+    { input: "255.100.50.0", expected: "255[.]100[.]50[.]0" },
+    { input: "192.168.1.1", expected: "192[.]168[.]1[.]1" },
+  ];
+  testCases.forEach(({ input, expected }) => {
+    it(`should defang the IP address ${input}`, () => {
+      expect(defangIPaddr(input)).toBe(expected);
+    });
   });
 });
